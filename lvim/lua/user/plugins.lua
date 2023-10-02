3664a314ab6ad938495a5b7f4100a99e203573b9
Dylan Wax <djwax12@gmail.com>
2023-10-02 11:20:50 -0400

    changes

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -1,6 +1,7 @@
 lvim.builtin.nvimtree.active = false
 
 lvim.plugins = {
+    { "udalov/kotlin-vim" },
     { "MunifTanjim/nui.nvim" },
     { "ThePrimeagen/harpoon" },
     { "roobert/activate.nvim" },
--- system_scripts/carti.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl 'https://lyric.mackle.im/api?artist=carti' | jq . > $HOME/.cache/carti &
