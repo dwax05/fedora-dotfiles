d8a2fc6d71e96cc98c12a2f1deea0bb5c48faf0a
Dylan Wax <djwax12@gmail.com>
2023-10-08 18:52:08 -0400

    BATMAN

--- lvim/lua/user/keybindings.lua
+++ lvim/lua/user/keybindings.lua
@@ -13,7 +13,7 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "'", "\"")
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
